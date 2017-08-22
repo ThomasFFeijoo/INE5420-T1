2c70d2a9ff7a41e90fb54f4094d0fecca5a7281a
Thomas <thomas@zilha.com.br>
2017-08-21 23:13:59 -0300

    world created

---
+++ include/World.hpp
@@ -0,0 +1,21 @@
+#ifndef INCLUDE_WORLD_HPP_
+#define INCLUDE_WORLD_HPP_
+
+#include "DisplayFile.hpp"
+#include "Matriz.hpp"
+
+class World {
+private:
+	DisplayFile world_displayFile;
+public:
+	World(){}
+	~World(){}
+	bool adicionaObjetosNoMundo(Objeto* obj);
+	void removeObjetosNoMundo(std::string nome);
+	void destroiTodosObjetosDoMundo();
+	DisplayFile* getDisplayfile(){return &world_displayFile;}
+	void transformarObjeto(std::string nome, Matriz::Matriz<double> matriz);
+	void scalonarObjeto(std::string nome, Matriz::Matriz<double> matriz);
+	void rotacionarObjeto(std::string nome, bool coordArbitraria, Coordenadas coords, Matriz::Matriz<double> matriz);
+};
+#endif /* INCLUDE_WORLD_HPP_ */
